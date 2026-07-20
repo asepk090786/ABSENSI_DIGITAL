f0e5f7e54610deac8f94813d30775fb5f1eb84f5
asepk090786 <asepk090786@users.noreply.github.com>
2026-07-20 16:58:24 +0700

    fix(view): label 'Deskripsi' -> 'Capaian Pembelajaran' on rencana_pembelajaran create

--- resources/views/rencana_pembelajaran/create.blade.php
+++ resources/views/rencana_pembelajaran/create.blade.php
@@ -51,7 +51,7 @@
                         </div>
 
                         <div class="col-md-12 mb-2">
-                            <label class="form-label">Deskripsi</label>
+                            <label class="form-label">Capaian Pembelajaran</label>
                             <textarea name="deskripsi" class="form-control @error('deskripsi') is-invalid @enderror" rows="3">{{ old('deskripsi') }}</textarea>
                             @error('deskripsi')
                                 <div class="invalid-feedback">{{ $message }}</div>
