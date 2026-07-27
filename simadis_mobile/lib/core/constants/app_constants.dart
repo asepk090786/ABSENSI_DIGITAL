7cd23a7e503e524e6ca0beecd4a931a7a0ae73c9
asepk090786 <asepk090786@users.noreply.github.com>
2026-07-27 12:19:57 +0700

    Remove simadis_mobile and android_app directories from repository

--- simadis_mobile/lib/core/constants/app_constants.dart
+++
@@ -1,7 +0,0 @@
-class AppConstants {
-  static const String appName = 'SIMADIS';
-  static const String storageTokenKey = 'auth_token';
-  static const String storageUserKey = 'auth_user';
-  static const int defaultPageSize = 15;
-  static const Duration defaultTimeout = Duration(seconds: 30);
-}
